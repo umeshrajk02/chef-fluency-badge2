fc649d26f1691447a61553756d4c3a283bc4c45b
root <root@aravindansurendran1.mylabserver.com>
2018-06-06 02:55:54 +0000

    added postgresql cookbook

---
+++ cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: postgresql
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]'
+end
+
+execute 'postgresql-init' do
+	command 'postgresql-set-up initdb'
+	action :nothing
+end
+
+service 'postgresql-service' do
+	service_name 'postgresql'
+	action [:enable, :start]
+end
